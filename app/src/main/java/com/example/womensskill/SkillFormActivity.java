e76493d5a6dd84dd00b83328e1256389dcf96cf6
Bilal-Technolligence <nicemehar24@gmail.com>
2021-03-06 05:09:59 +0500

    Form added

---
+++ app/src/main/java/com/example/womensskill/SkillFormActivity.java
@@ -0,0 +1,25 @@
+package com.example.womensskill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SkillFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_form);
+
+//        urdu=(EditText) findViewById(R.id.urdu);
+//        try
+//        {
+//            urdu.setTypeface(Typeface.createFromAsset(this.getAssets(),"urdu.ttf"));
+//            urdu.setText("ur text");
+//        }
+//        catch(Exception ex)
+//        {
+//            start.setText(ex.toString());
+//        }
+    }
+}

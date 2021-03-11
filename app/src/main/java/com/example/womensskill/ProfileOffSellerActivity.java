b4ded9bd550e7835cc3eadd5223b67eb11103c2b
Bilal-Technolligence <nicemehar24@gmail.com>
2021-03-11 07:40:49 +0500

    Form added

--- app/src/main/java/com/example/womensskill/ProfileOffSellerActivity.java
+++ app/src/main/java/com/example/womensskill/ProfileOffSellerActivity.java
@@ -7,22 +7,33 @@ import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
-public class ProfileOffSellerActivity extends AppCompatActivity {
+public class ProfileOffSellerActivity extends BaseClass {
     Switch btnSellerMode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_off_seller);
+       // setContentView(R.layout.activity_profile_off_seller);
         ((AppCompatActivity)this).getSupportActionBar().setTitle("Seller Mode");
         ((AppCompatActivity)this).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        btnSellerMode.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                if(!btnSellerMode.isChecked()){
-//                    startActivity(new Intent(getApplicationContext(),ProfileDetailsActivity.class));
-//                }
-//
-//            }
-//        } );
+        btnSellerMode = (Switch) findViewById(R.id.btnswitchSeller);
+        btnSellerMode.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!btnSellerMode.isChecked()){
+                    startActivity(new Intent(getApplicationContext(),ProfileDetailsActivity.class));
+                }
+
+            }
+        } );
+    }
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_profile_off_seller;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.nav_profile;
     }
 }
